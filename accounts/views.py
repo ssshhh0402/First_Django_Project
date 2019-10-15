d69a0b84205da2bc2b812c8884727fd7303fa1b8
ssshhh0402 <ssshhh0402@naver.com>
2019-10-15 14:17:52 +0900

    20191015|로그아웃

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 # Create your views here.
 
 def signup(request):
@@ -15,3 +17,22 @@ def signup(request):
     }
     return render(request,'accounts/signup.html', context)
     
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            #로그인
+            auth_login(request, form.get_user())
+            redirect ('articles:index')
+    else:
+        form = AuthenticationForm()
+    
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
